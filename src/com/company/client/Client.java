4b3128b29bd0cfbc6ea2dfaf69a33911e474645b
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2021-11-30 18:34:50 +0100

    Init
    I'm not sure if maven or gradle is allowed, next step is to setup them

---
+++ src/com/company/client/Client.java
@@ -0,0 +1,7 @@
+package com.company.client;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Client is running...");
+    }
+}
---
+++ src/com/company/server/Server.java
@@ -0,0 +1,7 @@
+package com.company.server;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server is running...");
+    }
+}
